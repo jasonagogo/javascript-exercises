1fae501efdc30169fc426e395c8a4e1ea748b0e0
jasonagogo <jasonagogo@gmail.com>
2023-03-28 13:18:02 -0400

    add initial test to enter loop and inoperable loop (final arg only)

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function(word) {
+let string = word;
+let n = parseInt(string.length);
+let newString ='';
 
+for (i = n - 1; i >= 0; i--) {
+    newString += string.charAt(i);
+}
+return newString;
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
 const removeFromArray = function() {
-
-};
+  let array = Array.from(arguments[0]);
+  let num = arguments.length;
+  let newArray = array;
+ // console.log(array)
+ // console.log(num);
+  if (arguments.length <= 1) {
+    return array;
+  } else{
+    for (i = num; i > 1; i--) {
+     newArray = newArray.filter(poop => poop != arguments[i-1])
+     return newArray;
+    }
+    /*console.log(newArray)*/
+  }
+}
+/*console.log(removeFromArray([1,2,3,4], 4));*/
 
 // Do not edit below this line
 module.exports = removeFromArray;
