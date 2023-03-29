99cf1035d61ff83b3d8ac6a6458e7fc88521ac4a
jasonagogo <jasonagogo@gmail.com>
2023-03-29 11:03:00 -0400

    add func to test for leap year truth using nested if statements

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
 const leapYears = function() {
-
+let year = parseInt(arguments[0]);
+if (year % 4 === 0) {
+  if (year % 100 === 0) {
+    if(year % 400 === 0) {
+        return true;
+    }
+    return false;
+  }
+  return true;
+} else {
+    return false;
+}
 };
 
 // Do not edit below this line
